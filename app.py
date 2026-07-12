ef25253b4f43645f7e6780946a12a1f03a6f6270
kittisak2527 <kkk_mooy@hotmail.com>
2026-07-12 13:18:02 +0700

    Change BTCUSD icon from 💎 to ☢

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="เลขาตลาด • ทอง / BTC / EU
 pages = {
     "📊 Technical": [
         st.Page("gold_page.py", title="ทองคำ", icon="🥇", default=True),
-        st.Page("btc_page.py", title="BTCUSD", icon="💎"),
+        st.Page("btc_page.py", title="BTCUSD", icon="☢"),
         st.Page("euro_page.py", title="EUR/USD", icon="💶"),
     ],
     "📰 Fundamental": [
